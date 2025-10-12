f3365ed1e1a0519cfcf46f539412ab6bba71d96f
shubham0419 <shubham.e21@nsut.ac.in>
2025-10-13 01:52:15 +0530

    build fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.232.87.201:4000/api/:path*', 
+      },
+    ]
+  },
 };
 
 export default nextConfig;
